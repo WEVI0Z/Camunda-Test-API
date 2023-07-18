93bdc3ae4222ad48d56d92e272637a6ecde89592
wevioz <wevioz@mail.ru>
2023-07-18 11:43:02 +0300

    Initial commit

---
+++ src/main/java/ru/wevioz/PrepareToBattle.java
@@ -0,0 +1,33 @@
+package ru.wevioz;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrepareToBattle implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        int warriors = (int) delegateExecution.getVariable("warriors");
+
+        int enemyWarriors = (int) (Math.random() * 100);
+        String battleStatus = "undefined";
+        boolean isWin = false;
+
+        if (warriors < 1 || warriors > 100) {
+            throw new BpmnError("warriorsError");
+        }
+
+        if((warriors - enemyWarriors) > 0) {
+            isWin = true;
+            battleStatus = "Victory";
+        } else {
+            battleStatus = "Fail";
+        }
+
+        delegateExecution.setVariable("enemyWarriors", enemyWarriors);
+        delegateExecution.setVariable("battleStatus", battleStatus);
+        delegateExecution.setVariable("isWin", isWin);
+    }
+}
